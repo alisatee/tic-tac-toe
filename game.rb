9ff35d6245ee55319e4ea6f8392b7e966831c06a
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-11-08 18:40:17 -0800

    Set up skeleton for Game model

---
+++ game.rb
@@ -0,0 +1,45 @@
+module Tictactoe
+
+class Game
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
+  end
+
+  def start_game
+
+  end
+
+  def reset_game
+
+  end
+
+  def game_over?
+
+  end
+
+  def get_winner
+
+  end
+
+  def check_if_win
+
+  end
+
+  private 
+
+  def check_row
+
+  end
+
+  def check_column
+
+  end
+
+  def check_diagonal
+
+  end
+end
+
+end
