78a6efe9b4575cf2887ae85abe7a92902ad6b9d0
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-11-09 17:45:14 -0800

    Moved win logic to its own class for clarity

--- game.rb
+++ game.rb
@@ -1,12 +1,12 @@
 module Tictactoe
-
+require_relative "winlogic"
 class Game
   attr_reader :board, :current_player, :winner 
   def initialize(player1, player2, board)
     @player1 = player1
     @player2 = player2
-    @winner = false 
     @board = board
+    @check_win = Win_logic.new(@board)
   end
 
   def start_game
@@ -18,7 +18,7 @@ class Game
   end
 
   def make_move(player, move_coordinates)
-    @board.play_piece(player.player_piece, move_coordinates)
+      @board.play_piece(player.player_piece, move_coordinates)
   end
 
   def switch_players
@@ -26,15 +26,12 @@ class Game
   end
 
   def game_over?
+    p "game over?"
     @winner || check_if_draw
   end
 
-  def get_winner
-    
-  end
-
   def check_if_win(player, move_coordinates)
-    if check_row(player, move_coordinates) || check_column(player, move_coordinates) || check_diagonals(player, move_coordinates)
+    if @check_win.winner_exists?(player, move_coordinates)
       @winner = @current_player
     end
   end
@@ -42,66 +39,7 @@ class Game
   def check_if_draw 
     @board.is_full?
   end
-
-  private 
-
-  def board_width
-    @board.width_of_board
-  end
-
-  def check_row(player_piece, move_coordinates)
-    row = move_coordinates.first
-    @board.game_board[row].each do |space| 
-      if space != player_piece
-        return false
-        break
-      end 
-    end
-    true 
-  end
-
-  def check_column(player_piece, move_coordinates)
-    column_transposed = move_coordinates.last
-    @board.game_board.transpose[column_transposed].each do |space|
-       if space != player_piece
-        return false
-        break
-      end 
-    end
-    true 
-  end
-
-  def check_diagonals(player_piece, move_coordinates)
-    check_southeast_diag(player_piece) || check_northeast_diag(player_piece)
-  end
-
-  def check_southeast_diag(player_piece)
-    i = 0 
-    while i < board_width do 
-      if @board.game_board[i][i] != player_piece
-        return false
-        break
-      end
-     i += 1
-    end
-    true
-  end
-
-  def check_northeast_diag(player_piece)
-    i = 0
-    while i < board_width do 
-      if @board.game_board[i][(board_width - 1) - i] != player_piece
-        return false
-        break
-      end
-      i += 1
-    end
-    true 
-  end
+  
 end
 
-end
-
-if __FILE__ == $0
- board = Tictactoe::Board.new(3).is_full?
 end
---
+++ winlogic.rb
@@ -0,0 +1,70 @@
+module Tictactoe
+
+class Win_logic
+
+  def initialize(board)
+    @board = board
+  end
+
+  def winner_exists?(player, move_coordinates)
+    check_row(player, move_coordinates) || check_column(player, move_coordinates) || check_diagonals(player, move_coordinates)
+  end
+
+  private 
+
+  def board_width
+    @board.width_of_board
+  end
+
+  def check_row(player_piece, move_coordinates)
+      row = move_coordinates.first
+      @board.game_board[row].each do |space| 
+        if space != player_piece
+          return false
+          break
+        end 
+      end
+      true 
+  end
+
+  def check_column(player_piece, move_coordinates)
+    column_transposed = move_coordinates.last
+    @board.game_board.transpose[column_transposed].each do |space|
+       if space != player_piece
+        return false
+        break
+      end 
+    end
+    true 
+  end
+
+  def check_diagonals(player_piece, move_coordinates)
+    check_southeast_diag(player_piece) || check_northeast_diag(player_piece)
+  end
+
+  def check_southeast_diag(player_piece)
+    i = 0 
+    while i < board_width do 
+      if @board.game_board[i][i] != player_piece
+        return false
+        break
+      end
+     i += 1
+    end
+    true
+  end
+
+  def check_northeast_diag(player_piece)
+    i = 0
+    while i < board_width do 
+      if @board.game_board[i][(board_width - 1) - i] != player_piece
+        return false
+        break
+      end
+      i += 1
+    end
+    true 
+  end
+
+end
+end
