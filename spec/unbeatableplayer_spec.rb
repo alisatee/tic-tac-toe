9aae98b0779b1679b379a429ab1c541b42a30829
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-11-16 21:41:20 -0800

    spec for unbeatable player / minimax

---
+++ spec/unbeatableplayer_spec.rb
@@ -0,0 +1,19 @@
+require_relative "spec_helper"
+module Tictactoe
+  describe Unbeatable_player do 
+    before :each do 
+      @board = Board.new(3)
+    end
+  context '#make_best_move' do 
+    it "should always result in a tie when playing against itself" do 
+      @game = Game.new(@board, Unbeatable_player.new("O"), Unbeatable_player.new("X")) 
+      @game.start_game
+      until @game.over? do 
+        @game.current_player.make_best_move(@game)
+        @game.switch_players
+      end
+      expect(@game.draw?).to eq(true)
+    end
+  end
+  end
+end
